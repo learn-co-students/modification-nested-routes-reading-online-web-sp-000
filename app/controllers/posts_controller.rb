b68f1dcdb3ebf83bbc9c3f00023f6391aab6fae7
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-05 19:18:05 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,9 +16,16 @@ class PostsController < ApplicationController
     end
   end
 
-  def new
-    @post = Post.new
+#We have the route, now update new action to handle the :author_id param
+#if we capture author_id through a nested route, we keep track of it
+# and assign the post to that author
+def new
+  if params[:author_id] && !Author.exists?(params[:author_id]) #check for params[:author_id] and see if author exists 
+    redirect_to authors_path, alert: "Author not found."
+  else
+    @post = Post.new(author_id: params[:author_id]) #creating a new post for a valid author
   end
+end
 
   def create
     @post = Post.new(post_params)
@@ -27,18 +34,33 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to post_path(@post)
-  end
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to post_path(@post)
+end
 
-  def edit
-    @post = Post.find(params[:id])
+#check if author_id is valid and the post matches the author
+def edit
+if params[:author_id] #comes from our nested route
+  author = Author.find_by(id: params[:author_id]) #find valid author
+  if author.nil?
+    redirect_to authors_path, alert: "Author not found." #flash alert if can't find valid author
+  else
+    @post = author.posts.find_by(id: params[:id]) #if author valid, find post by params[:id], query through our author.posts
+    redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
   end
+else
+  @post = Post.find(params[:id])
+end
+end
 
   private
 
+#strong params
+#post controller must accept :author_id as a param for a post
+#strong params = security practice to help prevent allowing users to update sensitive model attributes
+# author_id allowed for mass assignment in the create action
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:show, :index] do
+    resources :posts, only: [:show, :index, :new, :edit]
+  end
+  resources :posts
 end
+
+# creating a new post for an author
+# create a new post that is linked to an author
+# before: nested resources to view posts by an author
+# now: nested resources to create posts by author by adding :new
+
+# access to /authors/:author_id/posts/new, and a new_author_post_path helper
+
+# rake routes:
+# url pattern: /authors/:author_id/posts/new = posts#new controller action
+
+#editing an authors posts
+#:edit action in the nested route 
