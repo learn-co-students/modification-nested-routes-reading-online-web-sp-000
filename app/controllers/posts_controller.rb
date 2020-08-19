47ba1dab051545a07341e379dff8ab06b17d51e8
chienleow <chienleow1@gmail.com>
2020-08-18 23:38:04 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,10 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = Post.new(author_id: params[:author_id])
+    # passing the params[:author_id] into Post.new()
+    # we want to make sure that if we capture an author_id through a nested route
+    # we keep track of it and assign the post to that author.
   end
 
   def create
@@ -39,6 +42,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
