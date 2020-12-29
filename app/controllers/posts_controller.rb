f838cc3b68bbade14bf1a848ed71e5abbad700bb
javonesm89 <javosmith12@gmail.com>
2020-12-29 16:50:50 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   def index
     if params[:author_id]
       @posts = Author.find(params[:author_id]).posts
+      # redirect_to author_posts_path()
     else
       @posts = Post.all
     end
@@ -10,14 +11,19 @@ class PostsController < ApplicationController
 
   def show
     if params[:author_id]
-      @post = Author.find(params[:author_id]).posts.find(params[:id])
+      @author = Author.find(params[:author_id])
+      @post = @author.posts.find(params[:id])
     else
       @post = Post.find(params[:id])
     end
   end
 
   def new
-    @post = Post.new
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = Post.new(author_id: params[:author_id])
+    end
   end
 
   def create
@@ -25,20 +31,31 @@ class PostsController < ApplicationController
     @post.save
     redirect_to post_path(@post)
   end
-
+  
+  def edit
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else
+        @post = author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end
+    else
+      @post = Post.find(params[:id])
+    end
+  end
+  
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post)
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
